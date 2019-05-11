dda7bc067faaeb946f3a47abb9f3d1ad30924388
MIsha <misha_kazeev@mail.ru>
2019-05-11 12:23:29 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,2 +1,12 @@
-Crafty.init(500,350, document.getElementById('game'));
-Crafty.e('2D, DOM, Color').attr({x: 0, y: 0, w: 100, h: 100}).color('#F00');
+
+Crafty.init(300,300, document.getElementById('game'));
+
+Crafty.e('Floor, 2D, Canvas, Color')
+    .attr({x: 0, y: 250, w: 250, h: 10})
+    .color('green');
+
+Crafty.e('2D, Canvas, Color, Twoway, Gravity')
+    .attr({x: 0, y: 0, w: 50, h: 50})
+    .color('#F00')
+    .twoway(200)
+    .gravity('Floor');
